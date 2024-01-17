33b5fb70f8d365f331b9132856b5002d2aae8e5a
Arnaud-Lmr <145463981+Arnaud-Lmr@users.noreply.github.com>
2024-01-17 16:56:25 +0100

    Update Purchase.jsx

--- src/pages/Purchase.jsx
+++ src/pages/Purchase.jsx
@@ -165,7 +165,7 @@ function Purchase({ shoppingList, setShoppingList }) {
             </div>
             <img
               className={styles.imgCheck}
-              src="../public/checkCircle.png"
+              src="checkCircle.png"
               alt="ok logo"
             ></img>
           </div>
